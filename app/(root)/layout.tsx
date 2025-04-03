c9156f9e67e7bad2e81adcb8aebf11e2f76f468d
Pranav R <pranav81234@gmail.com>
2025-04-03 13:18:48 +0530

    home page ui completed

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,26 @@
-import React, { ReactNode } from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React, { ReactNode } from "react";
 
-const RootLayout = ({children}: {children: ReactNode}) => {
+const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div>{children}</div>
-  )
-}
+    <div className="root-layout">
+      <nav>
+        <Link href='/' className="flex items-center gap-4">
+          <Image 
+            src='/logo.svg'
+            alt="logo"
+            width={28}
+            height={32}
+          />
+          <h2 className="text-primary-100">
+            NextStep AI
+          </h2>
+        </Link>
+      </nav>
+      {children}
+    </div>
+  );
+};
 
-export default RootLayout
+export default RootLayout;
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,47 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { interviewCovers, mappings } from "@/constants";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+const techIconBaseUrl = "https://cdn.jsdelivr.net/gh/devicons/devicon/icons"
+
+const normalizedTechName = (tech: string) => {
+  const key = tech.toLowerCase().replace(/\.js$/, "").replace(/\s+/g, "")
+  return mappings[key as keyof typeof mappings]
+};
+
+const checkIconExists = async(url: string) =>{
+  try {
+    const response = await fetch(url, {method: "HEAD"})
+    return response.ok
+  } catch (error) {
+    return false
+  }
+}
+
+export const getTechLogos = async (techArray: string[]) => {
+  const logoUrl = techArray.map((tech) => {
+    const normalized = normalizedTechName(tech);
+    return {
+      tech,
+      url: `${techIconBaseUrl}/${normalized}/${normalized}-original.svg`
+    }
+  });
+
+  const results = await Promise.all(
+    logoUrl.map(async ({tech, url}) => ({
+      tech,
+      url: (await checkIconExists(url)) ? url : "tech.svg"
+    }))
+  )
+
+  return results
+};
+
+export const getRandomInterviewCover = () => {
+  const randomIndex = Math.floor(Math.random() * interviewCovers.length);
+  return `/covers${interviewCovers[randomIndex]}`;
+};
